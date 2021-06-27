3c391f0f6bbb9e7feea3a33fd1b5de56985ec36b
Arik Perlmutter <arik.perlmutter@walkme.com>
2021-06-27 16:39:49 +0300

    change outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instances_ids" {
-  value = aws_instance.my_server[*].id
+  value = aws_instance.my_ubuntu[*].id
 }
 
 output "instances_public_ips" {
-  value = aws_instance.my_server[*].public_ip
+  value = aws_instance.my_ubuntu[*].public_ip
 }
