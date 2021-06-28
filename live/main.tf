7b682e7b80f7e6188ed102d69ce058a772f7756a
Arik Perlmutter <arik.perlmutter@walkme.com>
2021-06-28 15:49:43 +0300

    commit to STG

--- live/main.tf
+++ live/main.tf
@@ -1,6 +1,6 @@
 module "vpc_dev" {
   source                           = "../modules/vpc"
-  env                              = "DEV"                         
+  env                              = "STAGING"                         
   cidr                             = "10.101.0.0/16"
   instance_tenancy                 = "default"
   enable_dns_hostnames             = true
